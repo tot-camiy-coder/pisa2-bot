0228ef4e5a2386a37a9208334af79eace397b139
tot-camiy-coder <mfua.crack@gmail.com>
2026-01-02 02:50:30 +0300

    add stickers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 import sys
+import random
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
@@ -25,6 +26,15 @@ dp = Dispatcher()
 dp.include_router(admin_router)  # Админ роутер первым, чтобы перехватывать команды
 dp.include_router(user_router)
 
+def get_random_welcome_sticker():
+    stickers = ["CAACAgIAAxkBAAEU3JBpSyNlsQ5lBKzKMxdy-fozh-poNQAC9SwAApB_iElQpWlBK-7ghzYE", "CAACAgQAAxkBAAEU3I5pSyNWNe1Lqe_vR0TBST_B0IPlLwACyAoAAuBWgVDzFIAWz9caRzYE",
+                "CAACAgQAAxkBAAEU3IxpSyNTmrSOSE_6RoMJgAcTbdZb-gACLA4AAkergVAfseRQjo3VVDYE", "CAACAgIAAxkBAAEU3IppSyNHFI4CZGGe25hNh2nJpXm5JAACLVYAAlx4QEvGY5AYemj_gzYE",
+                "CAACAgIAAxkBAAEU3IhpSyNFBrVBljspGVNwno0UDRAbpAACilQAAs9_SUsS79c03q2WYjYE", "CAACAgIAAxkBAAEU3IJpSyMxeevsecjHx6BlPVFmVpKzxgAClDgAAnwXCUmZu5mTOKoNsDYE",
+                "CAACAgIAAxkBAAEU3JJpSyQQVqO9CmVTAsfui3a_FzeT3gACIhEAAuegsUvoorIci0ypVjYE", "CAACAgIAAxkBAAEU3JRpSyQj4oBz74v0u8cpieMS7kQinAACBxQAAjq8GUnb-7dSZcigwzYE",
+                "CAACAgIAAxkBAAEU3JhpSyQ0phMVJpu0mxthvtJCERWrewACNhQAAjKsAUh2R0xw5X8U1zYE", "CAACAgIAAxkBAAEU3JxpSyRNdrLM6Uicz65D_U_SO3_tdAAC82oAAm7OGEjI0Xnb43CjkDYE",
+                "CAACAgIAAxkBAAEU3J5pSyRgSLOJjyg7wYrXEaH7Y64VEgACM2cAAqj7GEgS9kty0z-3FjYE", "CAACAgIAAxkBAAEU3KBpSySFxmsPlZSLCCR4mvSR6WTX_QACuygAAitGsEnkViOfu2Bo5DYE",
+                "CAACAgIAAxkBAAEU3KJpSyS2KOLvcLKXgU2juiPRX-T1cQACyG0AAkYUUUtq-y81tn33uDYE", "CAACAgIAAxkBAAEU3KRpSyS9pSm54HS4S9BDqT4w2HcMygACTWgAAuj48Et3--gt0X1TZDYE"]
+    return random.choice(stickers)
 
 @dp.message(CommandStart())
 async def start(message: Message):
@@ -49,8 +59,9 @@ async def start(message: Message):
     
     builder.adjust(1, 2, 2, 1, 1)  # Добавил ещё 1 для кнопки админки
     
+    await message.answer_sticker(get_random_welcome_sticker())
     await message.answer(
-        "📖 Менюшка:",
+        "👋 Привет! Добро пожаловать в официального бота @loltrains",
         reply_markup=builder.as_markup()
     )
 
