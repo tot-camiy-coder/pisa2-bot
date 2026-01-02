8cbb42bdfd006c4f794d2f5fa290c3412f0825bb
tot-camiy-coder <mfua.crack@gmail.com>
2026-01-02 20:29:41 +0300

    upd bot (add role admin)

--- handlers/admin.py
+++ handlers/admin.py
@@ -297,7 +297,9 @@ async def view_profile(callback: CallbackQuery, state: FSMContext, bot: Bot):
         text += f"━━━ <b>Данные в боте</b> ━━━\n"
         text += f"📛 <b>Сохранённое имя:</b> {db_user.full_name or '—'}\n"
         text += f"👤 <b>Сохранённый @:</b> @{db_user.username or '—'}\n"
-        text += f"🚫 <b>Статус:</b> {'☠ Забанен' if db_user.banned else '🟢 Активен'}\n"
+        text += f"🚫 <b>Статус:</b> {'💀 Забанен' if db_user.banned else '🟢 Активен'}\n"
+        if db_user.admin:
+            text += f"🎭 <b>Роль: Админ</b>"
         
         if db_user.registered_at:
             text += f"📅 <b>Первый контакт:</b> {db_user.registered_at.strftime('%d.%m.%Y %H:%M')}\n"
